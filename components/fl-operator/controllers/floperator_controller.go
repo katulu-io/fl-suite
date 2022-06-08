1c3924b28fcfb88479e879d0e0e5e6492f0cc1ff
Jan Schlicht <jan@katulu.io>
2022-06-08 14:25:57 +0200

    fix: add rbac for pods

--- components/fl-operator/controllers/floperator_controller.go
+++ components/fl-operator/controllers/floperator_controller.go
@@ -59,6 +59,7 @@ const (
 //+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
