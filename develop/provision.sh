3cdaa51fce532f2920e3b29255509a8aba35a9e2
Gerardo Gomez <gerardo@katulu.io>
2022-06-10 16:49:51 +0200

    chore(develop): override default kubeconfig to set the local develop k8s cluster

--- develop/provision.sh
+++ develop/provision.sh
@@ -3,18 +3,16 @@
 set -e
 
 CLUSTER_NAME=local.fl-suite
-kind create cluster --config=kind-config.yaml --kubeconfig "$CLUSTER_NAME.kubeconfig.yaml" || true
 
-# Export kind's internal kubeconfig to be used by devcontainer.
-# This kubeconfig file is kept hidden because it is only used by the provision script
-kind export kubeconfig --name "$CLUSTER_NAME" --internal --kubeconfig ".$CLUSTER_NAME-internal.kubeconfig.yaml"
+kind create cluster --config=kind-config.yaml || true
 
-KUBECONFIG="./$CLUSTER_NAME.kubeconfig.yaml"
-# If this script is running inside docker then use the internal kubeconfig
+# If this script is running inside docker (most probably devcontainer) then use a "internal" kubeconfig
 if grep -q docker /proc/self/cgroup; then
+  # This kubeconfig file is kept "hidden" because it is only used by the provision script
+  kind export kubeconfig --name "$CLUSTER_NAME" --internal --kubeconfig ".$CLUSTER_NAME-internal.kubeconfig.yaml"
   KUBECONFIG="./.$CLUSTER_NAME-internal.kubeconfig.yaml"
+  export KUBECONFIG
 fi
-export KUBECONFIG
 
 while ! kustomize build kustomize | kubectl apply -f -; do
   echo "Retrying to apply resources"
