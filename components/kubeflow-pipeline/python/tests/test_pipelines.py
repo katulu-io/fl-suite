6b7a9dc8044a4c1b512e48c5405a874ed4323686
Gerardo Gomez <gerardo@katulu.io>
2022-09-21 17:00:04 +0200

    refactor: simplify the pipeline unit tests to only use the task and pipeline concepts

--- components/kubeflow-pipeline/python/tests/test_pipelines.py
+++ components/kubeflow-pipeline/python/tests/test_pipelines.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import Any, Dict, List, Optional
 
 import numpy as np
@@ -7,6 +8,16 @@ from fl_suite.analytics import _correlation, data
 from fl_suite.pipelines import training_pipeline, fl_client
 
 
+@dataclass
+class Task:
+    """Test representation of a pipeline task"""
+
+    name: str
+    dependencies: Optional[List[str]] = None
+    parameters: Optional[Dict[str, str]] = None
+    artifacts: Optional[Dict[str, str]] = None
+
+
 def test_fl_client_training():
     """Tests the fl pipeline using the fl_client decorator"""
 
@@ -14,115 +25,119 @@ def test_fl_client_training():
     def client():
         pass
 
-    pipeline = training_pipeline(
-        fl_client=client,
-        registry="localhost:5000",
-        verify_registry_tls=False,
+    pipeline = parse_pipeline(
+        training_pipeline(
+            fl_client=client,
+            registry="localhost:5000",
+            verify_registry_tls=False,
+        )
     )
 
-    argo_workflow = convert_pipeline_to_argo_workflow(pipeline)
+    assert_exit_handler_in_pipeline(Task("cleanup-flower-server-infrastructure"), pipeline)
 
-    assert_argo_workflow_has_exit_handler(argo_workflow, "cleanup-flower-server-infrastructure")
-
-    dag = extract_dag(argo_workflow)
-
-    assert_task_is_setup(dag, "prepare-build-context")
-    assert_task_is_setup(
-        dag,
-        "fl-client-image-builder",
-        dependencies=["prepare-build-context"],
-        parameters={"image_tag": "{{inputs.parameters.image_tag}}"},
-        artifacts={
-            # pylint: disable-next=line-too-long
-            "prepare-build-context-build_context_path": "{{tasks.prepare-build-context.outputs.artifacts.prepare-build-context-build_context_path}}"  # noqa
-        },
+    assert_task_in_pipeline(Task("prepare-build-context"), pipeline)
+    assert_task_in_pipeline(
+        Task(
+            "fl-client-image-builder",
+            dependencies=["prepare-build-context"],
+            parameters={"image_tag": "{{inputs.parameters.image_tag}}"},
+            artifacts={
+                # pylint: disable-next=line-too-long
+                "prepare-build-context-build_context_path": "{{tasks.prepare-build-context.outputs.artifacts.prepare-build-context-build_context_path}}"  # noqa
+            },
+        ),
+        pipeline,
     )
 
-    assert_task_is_setup(dag, "setup-flower-server-infrastructure")
-    assert_task_is_setup(
-        dag,
-        "flower-server",
-        parameters={
-            "min_available_clients": "{{inputs.parameters.min_available_clients}}",
-            "min_eval_clients": "{{inputs.parameters.min_eval_clients}}",
-            "min_fit_clients": "{{inputs.parameters.min_fit_clients}}",
-            "num_local_rounds": "{{inputs.parameters.num_local_rounds}}",
-            "num_rounds": "{{inputs.parameters.num_rounds}}",
-        },
-        dependencies=["setup-flower-server-infrastructure"],
+    assert_task_in_pipeline(Task("setup-flower-server-infrastructure"), pipeline)
+    assert_task_in_pipeline(
+        Task(
+            "flower-server",
+            parameters={
+                "min_available_clients": "{{inputs.parameters.min_available_clients}}",
+                "min_eval_clients": "{{inputs.parameters.min_eval_clients}}",
+                "min_fit_clients": "{{inputs.parameters.min_fit_clients}}",
+                "num_local_rounds": "{{inputs.parameters.num_local_rounds}}",
+                "num_rounds": "{{inputs.parameters.num_rounds}}",
+            },
+            dependencies=["setup-flower-server-infrastructure"],
+        ),
+        pipeline,
     )
 
 
 def test_fl_client_context_url_training():
     """Tests the training pipeline using the client context url"""
 
-    pipeline = training_pipeline(
-        fl_client_context_url="a-non-existing-context-url",
-        registry="localhost:5000",
-        verify_registry_tls=False,
+    pipeline = parse_pipeline(
+        training_pipeline(
+            fl_client_context_url="a-non-existing-context-url",
+            registry="localhost:5000",
+            verify_registry_tls=False,
+        )
     )
 
-    argo_workflow = convert_pipeline_to_argo_workflow(pipeline)
+    assert_exit_handler_in_pipeline(Task("cleanup-flower-server-infrastructure"), pipeline)
 
-    assert_argo_workflow_has_exit_handler(argo_workflow, "cleanup-flower-server-infrastructure")
-
-    dag = extract_dag(argo_workflow)
-
-    assert_task_is_setup(dag, "prepare-build-context")
-    assert_task_is_setup(
-        dag,
-        "fl-client-image-builder",
-        dependencies=["prepare-build-context"],
-        parameters={"image_tag": "{{inputs.parameters.image_tag}}"},
-        artifacts={
-            # pylint: disable-next=line-too-long
-            "prepare-build-context-build_context_path": "{{tasks.prepare-build-context.outputs.artifacts.prepare-build-context-build_context_path}}"  # noqa
-        },
+    assert_task_in_pipeline(Task("prepare-build-context"), pipeline)
+    assert_task_in_pipeline(
+        Task(
+            "fl-client-image-builder",
+            dependencies=["prepare-build-context"],
+            parameters={"image_tag": "{{inputs.parameters.image_tag}}"},
+            artifacts={
+                # pylint: disable-next=line-too-long
+                "prepare-build-context-build_context_path": "{{tasks.prepare-build-context.outputs.artifacts.prepare-build-context-build_context_path}}"  # noqa
+            },
+        ),
+        pipeline,
     )
-    assert_task_is_setup(
-        dag,
-        "flower-server",
-        parameters={
-            "min_available_clients": "{{inputs.parameters.min_available_clients}}",
-            "min_eval_clients": "{{inputs.parameters.min_eval_clients}}",
-            "min_fit_clients": "{{inputs.parameters.min_fit_clients}}",
-            "num_local_rounds": "{{inputs.parameters.num_local_rounds}}",
-            "num_rounds": "{{inputs.parameters.num_rounds}}",
-        },
-        dependencies=["setup-flower-server-infrastructure"],
+    assert_task_in_pipeline(
+        Task(
+            "flower-server",
+            parameters={
+                "min_available_clients": "{{inputs.parameters.min_available_clients}}",
+                "min_eval_clients": "{{inputs.parameters.min_eval_clients}}",
+                "min_fit_clients": "{{inputs.parameters.min_fit_clients}}",
+                "num_local_rounds": "{{inputs.parameters.num_local_rounds}}",
+                "num_rounds": "{{inputs.parameters.num_rounds}}",
+            },
+            dependencies=["setup-flower-server-infrastructure"],
+        ),
+        pipeline,
     )
-    assert_task_is_setup(dag, "setup-flower-server-infrastructure")
+    assert_task_in_pipeline(Task("setup-flower-server-infrastructure"), pipeline)
 
 
 def test_fl_client_image_training():
     """Tests the training pipeline using a static client image"""
 
-    pipeline = training_pipeline(
-        fl_client_image="fl_client_image:latest",
-        registry="localhost:5000",
-        verify_registry_tls=False,
+    pipeline = parse_pipeline(
+        training_pipeline(
+            fl_client_image="fl_client_image:latest",
+            registry="localhost:5000",
+            verify_registry_tls=False,
+        )
     )
 
-    argo_workflow = convert_pipeline_to_argo_workflow(pipeline)
+    assert_exit_handler_in_pipeline(Task("cleanup-flower-server-infrastructure"), pipeline)
 
-    assert_argo_workflow_has_exit_handler(argo_workflow, "cleanup-flower-server-infrastructure")
-
-    dag = extract_dag(argo_workflow)
-
-    assert_task_is_setup(dag, "fl-client-image")
-    assert_task_is_setup(
-        dag,
-        "flower-server",
-        parameters={
-            "min_available_clients": "{{inputs.parameters.min_available_clients}}",
-            "min_eval_clients": "{{inputs.parameters.min_eval_clients}}",
-            "min_fit_clients": "{{inputs.parameters.min_fit_clients}}",
-            "num_local_rounds": "{{inputs.parameters.num_local_rounds}}",
-            "num_rounds": "{{inputs.parameters.num_rounds}}",
-        },
-        dependencies=["setup-flower-server-infrastructure"],
+    assert_task_in_pipeline(Task("fl-client-image"), pipeline)
+    assert_task_in_pipeline(
+        Task(
+            "flower-server",
+            parameters={
+                "min_available_clients": "{{inputs.parameters.min_available_clients}}",
+                "min_eval_clients": "{{inputs.parameters.min_eval_clients}}",
+                "min_fit_clients": "{{inputs.parameters.min_fit_clients}}",
+                "num_local_rounds": "{{inputs.parameters.num_local_rounds}}",
+                "num_rounds": "{{inputs.parameters.num_rounds}}",
+            },
+            dependencies=["setup-flower-server-infrastructure"],
+        ),
+        pipeline,
     )
-    assert_task_is_setup(dag, "setup-flower-server-infrastructure")
+    assert_task_in_pipeline(Task("setup-flower-server-infrastructure"), pipeline)
 
 
 def test_correlation_pipeline():
@@ -134,39 +149,42 @@ def test_correlation_pipeline():
 
     # For ease of testing, we test directly against the fl correlation_pipeline.
     # pylint: disable-next=protected-access
-    pipeline = _correlation.correlation_pipeline(
-        fl_client=client,
-        fl_server_image="ghcr.io/katulu-io/fl-suite/analytics-server:{__version__}",
-        registry="localhost:5000",
-        verify_registry_tls=False,
+    pipeline = parse_pipeline(
+        _correlation.correlation_pipeline(
+            fl_client=client,
+            fl_server_image="ghcr.io/katulu-io/fl-suite/analytics-server:{__version__}",
+            registry="localhost:5000",
+            verify_registry_tls=False,
+        )
     )
 
-    argo_workflow = convert_pipeline_to_argo_workflow(pipeline)
+    assert_exit_handler_in_pipeline(Task("cleanup-flower-server-infrastructure"), pipeline)
 
-    assert_argo_workflow_has_exit_handler(argo_workflow, "cleanup-flower-server-infrastructure")
-
-    dag = extract_dag(argo_workflow)
-    assert_task_is_setup(dag, "prepare-build-context")
-    assert_task_is_setup(
-        dag,
-        "fl-client-image-builder",
-        dependencies=["prepare-build-context"],
-        parameters={"image_tag": "{{inputs.parameters.image_tag}}"},
-        artifacts={
-            # pylint: disable-next=line-too-long
-            "prepare-build-context-build_context_path": "{{tasks.prepare-build-context.outputs.artifacts.prepare-build-context-build_context_path}}"  # noqa
-        },
+    assert_task_in_pipeline(Task("prepare-build-context"), pipeline)
+    assert_task_in_pipeline(
+        Task(
+            "fl-client-image-builder",
+            dependencies=["prepare-build-context"],
+            parameters={"image_tag": "{{inputs.parameters.image_tag}}"},
+            artifacts={
+                # pylint: disable-next=line-too-long
+                "prepare-build-context-build_context_path": "{{tasks.prepare-build-context.outputs.artifacts.prepare-build-context-build_context_path}}"  # noqa
+            },
+        ),
+        pipeline,
     )
-    assert_task_is_setup(
-        dag,
-        "flower-server",
-        parameters={"min_available_clients": "{{inputs.parameters.min_available_clients}}"},
-        dependencies=["setup-flower-server-infrastructure"],
+    assert_task_in_pipeline(
+        Task(
+            "flower-server",
+            parameters={"min_available_clients": "{{inputs.parameters.min_available_clients}}"},
+            dependencies=["setup-flower-server-infrastructure"],
+        ),
+        pipeline,
     )
-    assert_task_is_setup(dag, "setup-flower-server-infrastructure")
+    assert_task_in_pipeline(Task("setup-flower-server-infrastructure"), pipeline)
 
 
-def convert_pipeline_to_argo_workflow(pipeline) -> Dict[str, Any]:
+def parse_pipeline(pipeline) -> Dict[str, Any]:
     """Converts a pipeline to an argo workflow: dictionary representation"""
 
     compiler = Compiler()
@@ -175,50 +193,43 @@ def convert_pipeline_to_argo_workflow(pipeline) -> Dict[str, Any]:
     return compiler._create_workflow(pipeline)
 
 
-def extract_dag(argo_workflow: Dict[str, Any]) -> Dict[Any, Any]:
-    """Extracts the directed acylic graph from an argo workflow"""
-
-    entrypoint = find_by_name(
-        argo_workflow["spec"]["templates"], argo_workflow["spec"]["entrypoint"]
-    )
-    assert entrypoint is not None, "Entrypoint not found"
-
-    main_task = find_by_name(
-        argo_workflow["spec"]["templates"], entrypoint["dag"]["tasks"][0]["name"]
-    )
-    assert main_task is not None, "Main task not found"
-
-    return main_task["dag"]
-
-
-def assert_argo_workflow_has_exit_handler(argo_workflow: Dict[str, Any], exit_handler_name: str):
+def assert_exit_handler_in_pipeline(exit_handler: Task, parsed_pipeline: Dict[str, Any]):
     """Asserts that the given argo workflow has an exit handler"""
 
-    assert argo_workflow["spec"]["onExit"] == exit_handler_name
+    assert exit_handler.name == parsed_pipeline["spec"]["onExit"]
 
 
-def assert_task_is_setup(
-    dag: Dict[Any, Any],
-    task_name: str,
-    dependencies: Optional[List[str]] = None,
-    parameters: Optional[Dict[str, str]] = None,
-    artifacts: Optional[Dict[str, str]] = None,
+def assert_task_in_pipeline(
+    expected_task: Task,
+    pipeline: Dict[Any, Any],
 ):
-    """Asserts that the given task exists in the directed acylic graph"""
+    """Asserts that a given task exists in the pipeline"""
 
-    task = find_by_name(dag["tasks"], task_name)
-    assert task is not None, f'"{task_name}" task not found'
+    # Find and extracts the directed acylic graph from the parsed_pipeline
+    entrypoint = find_by_name(pipeline["spec"]["templates"], pipeline["spec"]["entrypoint"])
+    assert entrypoint is not None, "Entrypoint not found"
+    main_task = find_by_name(pipeline["spec"]["templates"], entrypoint["dag"]["tasks"][0]["name"])
+    assert main_task is not None, "Main task not found"
+    dag = main_task["dag"]
 
-    if dependencies is not None:
-        assert task["dependencies"] == dependencies
+    # Find and check that the task exists inside the dag
+    dag_task = find_by_name(dag["tasks"], expected_task.name)
+    assert dag_task is not None, f'"{expected_task.name}" task not found'
 
-    if parameters is not None:
-        expected_parameters = [{"name": name, "value": value} for name, value in parameters.items()]
-        assert task["arguments"]["parameters"] == expected_parameters
+    if expected_task.dependencies is not None:
+        assert dag_task["dependencies"] == expected_task.dependencies
 
-    if artifacts is not None:
-        expected_artifacts = [{"name": name, "from": value} for name, value in artifacts.items()]
-        assert task["arguments"]["artifacts"] == expected_artifacts
+    if expected_task.parameters is not None:
+        expected_parameters = [
+            {"name": name, "value": value} for name, value in expected_task.parameters.items()
+        ]
+        assert dag_task["arguments"]["parameters"] == expected_parameters
+
+    if expected_task.artifacts is not None:
+        expected_artifacts = [
+            {"name": name, "from": value} for name, value in expected_task.artifacts.items()
+        ]
+        assert dag_task["arguments"]["artifacts"] == expected_artifacts
 
 
 def find_by_name(template_list, template_name: str) -> Optional[Dict[Any, Any]]:
